bc549f89fc714e735b0b61d77f654c3d20089eed
Ananthu <ananthuanilkumar82@gmail.com>
2026-06-15 14:53:36 +0000

    alembic migrations

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from sqlmodel import SQLModel,Field
 class User(SQLModel,table =True):
     id:int|None=Field(default=None,primary_key=True)
     username:str
+    email:str
     
     hashed_password:str
 
@@ -33,6 +34,9 @@ class Image(SQLModel, table=True):
     filename: str
 
     owner_id: int =Field(foreign_key="user.id")
+    file_size: int|None =None
+
+    
 
 
     
